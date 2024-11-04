6f489472d1adcd47c955bc523a265ccc3075a21f
SamEnsey <sammiejonorman@gmail.com>
2024-11-04 14:14:39 -0600

    card style updates

--- src/components/ItemCard/ItemCard.jsx
+++ src/components/ItemCard/ItemCard.jsx
@@ -2,10 +2,10 @@ import "./ItemCard.css";
 
 function ItemCard({ item }) {
   return (
-    <div className="item-card">
+    <li className="item-card">
       <h2 className="item-card__name">{item.name}</h2>
       <img className="item-card__image" src={item.link} alt={item.name} />
-    </div>
+    </li>
   );
 }
 
