cd22fb615224058b5f290062c8d0053ae6357a29
SamEnsey <sammiejonorman@gmail.com>
2024-11-03 21:14:35 -0600

    add fonts

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,8 +6,8 @@ function Header() {
   return (
     <header className="header">
       <img className="header__logo" src={logo} alt="" />
-      <p className="header__date-and-location">DATE, LOCATION</p>
-      <button className="header__add-clothes-btn">+ Add Clothes</button>
+      <p className="header__date-and-location">June 15, New York</p>
+      <button className="header__add-clothes-btn">+ Add clothes</button>
       <div className="header__user-container">
         <p className="header__username">Terrence Tegegne</p>
         <img src={avatar} alt="Terrence Tegegne" className="header__avatar" />
