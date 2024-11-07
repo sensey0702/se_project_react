ab3589746421f4e12ea815bbb454a1f75bb39fa2
SamEnsey <sammiejonorman@gmail.com>
2024-11-06 23:57:54 -0600

    add Footer and update font

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+import "./Footer.css";
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <p className="footer__author">Developed by Samantha Ensey</p>
+      <p className="footer__copywright">2024</p>
+    </footer>
+  );
+}
+
+export default Footer;
