31c8573fa54f30b2344b96923c3143bf0781b320
SamEnsey <sammiejonorman@gmail.com>
2024-11-07 00:12:50 -0600

    footer update

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,8 +3,8 @@ import "./Footer.css";
 function Footer() {
   return (
     <footer className="footer">
-      <p className="footer__author">Developed by Samantha Ensey</p>
-      <p className="footer__copywright">2024</p>
+      <p className="footer__copywright">Developed by Samantha Ensey</p>
+      <p className="footer__year">2024</p>
     </footer>
   );
 }
