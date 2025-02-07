fc428d6a7c6b31ae824f105e0686a1747790577d
Sam Ensey <168656118+sensey0702@users.noreply.github.com>
2025-02-06 22:39:21 -0600

    update footer year dynamically

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,10 +1,11 @@
 import "./Footer.css";
 
 function Footer() {
+  const currentYear = new Date().getFullYear();
   return (
     <footer className="footer">
       <p className="footer__copywright">Developed by Samantha Ensey</p>
-      <p className="footer__year">2024</p>
+      <p className="footer__year">{currentYear}</p>
     </footer>
   );
 }
